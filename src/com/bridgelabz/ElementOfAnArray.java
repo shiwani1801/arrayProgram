a73f2ae57bfd3e8c0329f1f8172aee9fbf50b7bc
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 14:52:55 -0800

    [shiwani]added element of an array program

---
+++ src/com/bridgelabz/ElementOfAnArray.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class ElementOfAnArray {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 40, 50, 60};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
